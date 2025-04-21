fa591f1a3041643f0fca0a4182fbf5b81c442d52
gold496 <aldainewallace@gmail.com>
2025-04-21 02:43:27 -0500

    finnished basic functionality

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let humanScore =0;
 let computerScore = 0;
 
+let display = document.getElementById("message");
+let user = document.getElementById("user");
+let robot = document.getElementById("robot");
+
+
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
     switch (choice){
@@ -14,60 +19,89 @@ function getComputerChoice(){
     }
 }
 function getHumanChoice(){
-    return prompt("Enter your choice: ","Rock").toLowerCase().trim();
+    document.getElementById("rock").addEventListener("click",()=> {
+        playRound("rock",getComputerChoice());
+
+    })
+    document.getElementById("paper").addEventListener("click",()=>{
+
+        playRound("paper",getComputerChoice());
+
+    })
+    document.getElementById("scissors").addEventListener("click",()=>{ 
+
+        playRound("scissors",getComputerChoice());
+
+    });
+
 }
 
 function playRound(humanchoice,computerchoice){
     if (computerchoice === humanchoice){
         console.log("GAME DRAWN");
+        display.innerHTML ="Draw";
     }
     else if ((computerchoice === "rock") && (humanchoice === "paper")){
         console.log("you win");
         humanScore ++;
+        user.innerHTML=":"+humanScore;
     }
     else if ((computerchoice === "rock")){
         console.log("you lose");
+        display.innerHTML ="you lose";
         computerScore ++;
+        robot.innerHTML=":"+computerScore;
     }
     else if ((computerchoice === "paper") && (humanchoice === "scissors")){
         console.log("you win");
+        display.innerHTML ="you win";
         humanScore ++;
+        user.innerHTML=":"+humanScore;
         return;
     }
     else if ((computerchoice === "paper")){
         console.log("you lose");
+        display.innerHTML ="you lose";
         computerScore ++;
+        robot.innerHTML=":"+computerScore;
     }
     else if ((computerchoice === "scissors") && (humanchoice === "rock")){
         console.log("you win");
+        display.innerHTML ="you win";
         humanScore ++;
-        return;
+        user.innerHTML=":"+humanScore;
+        
     }
     else{
         console.log("you lose");
+        display.innerHTML ="you lose";
         computerScore ++;
+        robot.innerHTML=":"+computerScore;
     }
-    
-    
 
-}
-function playGame(){
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(),getComputerChoice());
-        
+    if (computerScore >=5) {
+        display.innerHTML ="Game over!! you lose";
+        document.location.reload();
+
+        return
     }
-    if (computerScore > humanScore) {
-        console.log("Game over!! you lose");
-    }
-    else if (computerScore < humanScore) {
+    else if (humanScore >=5) {
         console.log("Game over!! you win");
+        display.innerHTML ="Game over!! you Win!!";
+        document.location.reload();
+
+        return
     }
-    else{
-        console.log("Game over!! draw");
-    }
-   
     
+    
+
 }
 
-playGame();
+getHumanChoice();
+
+
+document.getElementById("start").addEventListener("click",()=>{
+    document.location.reload();
+
+});
 
